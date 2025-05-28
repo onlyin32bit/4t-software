486ef72ad246c6dc9378e081354ec52c500562d5
Bao Khang <onlyIn32bit@users.noreply.github.com>
2025-05-28 08:25:04 +0700

    Refactor display status subscription logic and improve code readability

--- src/routes/display/+page.svelte
+++ src/routes/display/+page.svelte
@@ -5,15 +5,15 @@
 
 	onMount(async () => {
 		await pb.collection('display_status').subscribe('4T-DISPLAYSTATE', ({ action, record }) => {
-			if (action === 'update') {
-				if (record.screen !== '') goto('/display/' + record.screen);
+			if (action === 'update' && record.screen !== '') {
+				goto('/display/' + record.screen);
 			}
 		});
 	});
 	onDestroy(() => pb.collection('display_status').unsubscribe('4T-DISPLAYSTATE'));
 </script>
 
-<div class="flex h-screen flex-col items-center justify-center gap-8">
+<div class="flex flex-col items-center justify-center h-screen gap-8">
 	<img src="src/lib/image/4t.png" alt="4T logo" class="h-[150px]" />
 	<span class="loading loading-ring loading-lg"></span>
 </div>
